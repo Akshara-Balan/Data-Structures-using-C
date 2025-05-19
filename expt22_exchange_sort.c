a7e504da64ebb32a3c1a8c964e9ea22dba47d321
Akshara Balan <aksharabalan0704@gmail.com>
2025-05-19 11:21:49 +0530

    Create expt22_exchange_sort.c

---
+++ expt22_exchange_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void exchangeSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                // swap
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i=0; i<n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    exchangeSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
